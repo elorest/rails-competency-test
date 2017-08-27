5ff2da1d5402be4bb8dfafd073cf8ff5c95106dc
Reece <reecebisel@gmail.com>
2017-08-27 16:27:33 -0600

    add in petergate model migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  petergate(roles: [:admin, :editor], multiple: false)
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
